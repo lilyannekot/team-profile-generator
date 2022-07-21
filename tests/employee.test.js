3ecbfaea61799bba2b6a088813daf955b3cfa35d
lilys tacos <lilyannek@gmail.com>
2022-07-21 16:58:19 -0700

    employee tests

---
+++ tests/employee.test.js
@@ -0,0 +1,11 @@
+const Employee = require("../lib/employee.js");
+
+describe("Employee", () => {
+  test("to see if the inputted information will appear in each section", () => {
+    const Jamie = new Employee("Jamie", "1", "jamie@helloworld.com");
+
+    expect(Jamie.name).toBe("Jamie");
+    expect(Jamie.id).toBe("1");
+    expect(Jamie.email).toBe("jamie@helloworld.com");
+  });
+});
