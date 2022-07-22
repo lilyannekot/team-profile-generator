a75fbfd8fe83b28f05b64cb0592855c6ad37317e
lilys tacos <lilyannek@gmail.com>
2022-07-21 19:48:20 -0700

    trying new layout for index.js and beginning of template.js

--- index.js
+++ index.js
@@ -29,15 +29,6 @@ const mandatoryQuestions = [
     message: "What is your manager's office number?",
   },
   {
-    type: "list",
-    name: "moreEmployees",
-    message: "What additional employee do you want to add?",
-    choices: ["Engineer", "Employee", "Intern"],
-  },
-];
-
-const engineerQuestions = [
-  {
     type: "input",
     name: "engineerName",
     message: "What is the name of your engineer?",
@@ -57,9 +48,6 @@ const engineerQuestions = [
     name: "github",
     message: "What is your engineer's Github username?",
   },
-];
-
-const internQuestions = [
   {
     type: "input",
     name: "internName",
@@ -80,9 +68,6 @@ const internQuestions = [
     name: "school",
     message: "What school does your intern go to?",
   },
-];
-
-const employeeQuestions = [
   {
     type: "input",
     name: "employeeName",
@@ -98,7 +83,77 @@ const employeeQuestions = [
     name: "employeeEmail",
     message: "What is your employee's email?",
   },
+  //   {
+  //     type: "list",
+  //     name: "moreEmployees",
+  //     message: "Do you want to add another employee?",
+  //     choices: ["Engineer", "Employee", "Intern", "No"],
+  //   },
 ];
+
+// const engineerQuestions = [
+//   {
+//     type: "input",
+//     name: "engineerName",
+//     message: "What is the name of your engineer?",
+//   },
+//   {
+//     type: "input",
+//     name: "engineerId",
+//     message: "What is your engineer's id number?",
+//   },
+//   {
+//     type: "input",
+//     name: "engineerEmail",
+//     message: "What is your engineer's email?",
+//   },
+//   {
+//     type: "input",
+//     name: "github",
+//     message: "What is your engineer's Github username?",
+//   },
+// ];
+
+// const internQuestions = [
+//   {
+//     type: "input",
+//     name: "internName",
+//     message: "What is the name of your intern?",
+//   },
+//   {
+//     type: "input",
+//     name: "internId",
+//     message: "What is your intern's id number?",
+//   },
+//   {
+//     type: "input",
+//     name: "engineerEmail",
+//     message: "What is your engineer's email?",
+//   },
+//   {
+//     type: "input",
+//     name: "school",
+//     message: "What school does your intern go to?",
+//   },
+// ];
+
+// const employeeQuestions = [
+//   {
+//     type: "input",
+//     name: "employeeName",
+//     message: "What is the name of your employee?",
+//   },
+//   {
+//     type: "input",
+//     name: "employeeId",
+//     message: "What is your employee's id?",
+//   },
+//   {
+//     type: "input",
+//     name: "employeeEmail",
+//     message: "What is your employee's email?",
+//   },
+// ];
 
 // Function to write HTML file
 function writeToFile(fileName, data) {
@@ -107,19 +162,21 @@ function writeToFile(fileName, data) {
   );
 }
 
-// Function to initialize app
+// Function to run questions
 function runQuestions() {
   inquirer.prompt(mandatoryQuestions).then((responses) => {
     console.log(responses);
 
-    if ((mandatoryQuestions.choices = "Engineer")) {
-      inquirer.prompt(engineerQuestions);
-    } else if ((mandatoryQuestions.choices = "Intern")) {
-      inquirer.prompt(internQuestions);
-    } else if ((mandatoryQuestions.choices = "Employee")) {
-      inquirer.prompt(employeeQuestions);
-    }
-    writeToFile("./dist/index.html");
+    // if (mandatoryQuestions.moreEmployees === "Engineer") {
+    //   inquirer.prompt(engineerQuestions);
+    // } else if (mandatoryQuestions.moreEmployees === "Intern") {
+    //   inquirer.prompt(internQuestions);
+    // } else if (mandatoryQuestions.moreEmployees === "Employee") {
+    //   inquirer.prompt(employeeQuestions);
+    // } else {
+    //   return "";
+    // }
+    writeToFile("dist/index.html", responses);
   });
 }
 
