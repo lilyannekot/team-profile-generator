89a3651a091c65f78b2a6c9c8b5618f16714d78a
lilys tacos <lilyannek@gmail.com>
2022-07-21 19:53:50 -0700

    layout for html

---
+++ src/template.js
@@ -0,0 +1,80 @@
+const createTeam = (team) => {
+  const createManager = (manager) => {
+    return `
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">
+                        Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
+                    </li>
+                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div> `;
+  };
+
+  const createEngineer = (engineer) => {
+    return `
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">
+                        Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
+                    </li>
+                    <li class="list-group-item">
+                        Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.geGgithub()}</a>
+                    </li>
+                </ul>
+            </div>
+        </div> `;
+  };
+
+  const createIntern = (intern) => {
+    return `
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">
+                        Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
+                    </li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li> 
+                </ul>
+            </div>
+        </div> `;
+  };
+
+  const createEmployee = (employee) => {
+    return `
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h2 class="card-title">${employee.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${employee.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${employee.getId()}</li>
+                    <li class="list-group-item">
+                        Email: <a href="mailto:${employee.getEmail()}">${intern.getEmail()}</a>
+                    </li>
+                </ul>
+            </div>
+        </div> `;
+  };
+};
+
+const html = [];
