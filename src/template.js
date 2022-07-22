873a673e8a7d333128fb61064b8e307ddea300d7
lilys tacos <lilyannek@gmail.com>
2022-07-21 19:56:51 -0700

    addition of employee types to html

--- src/template.js
+++ src/template.js
@@ -75,6 +75,35 @@ const createTeam = (team) => {
             </div>
         </div> `;
   };
-};
 
-const html = [];
+  const html = [];
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => createManager(manager))
+  );
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => createEngineer(engineer))
+      .join("")
+  );
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => createIntern(intern))
+      .join("")
+  );
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Employee")
+      .map((employee) => createEmployee(employee))
+      .join("")
+  );
+
+  return html.join("");
+};
